397f3ca4604e25e59f09002182d23ed8348068dd
Nils Brevier <nils.brevier@toulouse.miage.fr>
2019-11-29 13:59:23 +0100

    fix #1 Tests sur QuestionAChoixExclusif

--- src/test/java/miagem1/QuestionAChoixExclusifTest.java
+++ src/test/java/miagem1/QuestionAChoixExclusifTest.java
@@ -1,5 +1,46 @@
+package miagem1;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class QuestionAChoixExclusifTest {
+
+    private QuestionAChoixExclusif question;
+
+    @Before
+    public void setUp() throws Exception {
+        // given : un objet de type QuestionAChoixExclusif
+        question = new QuestionAChoixExclusif("L'année 2016 est une année bissextile. 1. Vrai, 2. Faux", 1);
+
+    }
+
+    @Test
+    public void testGetEnonce() {
+        // when : on demande l'ennoncé de la question
+        String enonce = question.getEnonce();
+        //then: l'ennoncé est non null
+        assertNotNull(enonce);
+        //and : l'ennoncé est bien celui fourni à la construction
+        assertEquals("L'année 2016 est une année bissextile. 1. Vrai, 2. Faux", enonce);
+    }
+
+    @Test
+    public void testGetScoreForIndice() {
+        //when : un étudiant fourni l'indice correspondant à la bonne réponse
+        int indiceEtudiant = 1;
+        //and : on demande le calcul du score
+        Float resScore = question.getScoreForIndice(indiceEtudiant);
+        //then : le score obtenu est 100
+        assertEquals(new Float(100f), resScore);
+
+        //when : un étudiant fourni l'indice correspondant à la mauvaise réponse
+        indiceEtudiant = 2;
+        //and : on demande le calcul du score
+        resScore = question.getScoreForIndice(indiceEtudiant);
+        //then : le score obtenu est 100
+        assertEquals(new Float(0f), resScore);
+    }
 
 }
